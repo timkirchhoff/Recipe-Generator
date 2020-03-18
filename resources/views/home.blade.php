374ca6cb8878eaffbf61a14fd6bb792bd7e022f6
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-03-18 13:56:23 -0500

    setup basic front-end template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,29 +8,56 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Recipe App</h1>
+						<p>Simplifying Your Dinner Decision Making Process</p>
+					</header>
+					<a href="#main" class="button big scrolly">Get Started</a>
+				</div>
+			</section>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+		<!-- Main -->
+			<div id="main">
+                <!-- Section for generating dinners -->
+                <section class="wrapper style1">
+                    <div class="inner">
+                        <div class="align-center">
+                            <h2>How many dinners do you want to make this week?</h2>
+                            <p>--FORM GOES HERE--</p>
+                        </div>
+                    </div>
+                </section>
+                    
+                <!-- Section for generating dinners -->
+                <section class="wrapper style2">
+                    <div class="inner">
+                        <div class="align-center">
+                            <h2>Have something yummy you would like to share?</h2>
+                            <a href="#" class="button big">Add New Recipe</a>
+                        </div>
+                    </div>
+                </section>
+			</div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Recipe App
-                </div>
-            </div>
-        </div>
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="copyright">
+					<p>&copy; Untitled. All rights reserved. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.</p>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="{{ asset('js/jquery.min.js') }}"></script>
+			<script src="{{ asset('js/jquery.scrolly.min.js') }}"></script>
+			<script src="{{ asset('js/jquery.scrollex.min.js') }}"></script>
+			<script src="{{ asset('js/skel.min.js') }}"></script>
+			<script src="{{ asset('js/util.js') }}"></script>
+			<script src="{{ asset('js/main.js') }}"></script>
     </body>
 </html>
