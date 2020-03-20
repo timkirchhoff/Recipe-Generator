6bceb147de91f2eb8cf51359b1ca813919cc81a4
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-03-20 13:27:39 -0500

    create ingredient model and migration

---
+++ database/migrations/2020_03_20_181820_create_ingredients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('recipe_id');
+            $table->string('name');
+            $table->string('amount');
+            $table->string('measurement');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
