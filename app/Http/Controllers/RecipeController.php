1a99c833e1d79eac8b1d05ea88ea283fb1df9955
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-03-23 14:05:00 -0500

    store recipe ingredients in ingredients table

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreRecipeRequest;
+use App\Ingredient;
 use App\Recipe;
 
 class RecipeController extends Controller
@@ -21,10 +22,11 @@ class RecipeController extends Controller
     {
         $validated_recipe = $request->validated();
         $recipe_info = array_slice($validated_recipe, 0, 2);
-        // $recipe_ingredients = array_slice($validated_recipe, 2, 1);
+        $recipe_ingredients = array_slice($validated_recipe, 2, 1);
         // $recipe_steps = array_slice($validated_recipe, 3, 1);
         
         $recipe = Recipe::storeRecipe($recipe_info);
+        Ingredient::storeIngredients($recipe->id, $recipe_ingredients);
         
         // dd($recipe->id);
         // return view('recipes.show')->with('data', $validated_recipe);
--- app/Ingredient.php
+++ app/Ingredient.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-    //
+    protected $fillable = ['recipe_id', 'name', 'amount', 'measurement'];
+
+    /**
+     * Store ingredients for a new recipe
+     *
+     * @param [type] $recipeId
+     * @param [type] $recipe_ingredients
+     * @return void
+     */
+    public static function storeIngredients($recipeId, $recipe_ingredients)
+    {
+        foreach($recipe_ingredients as $ingredients) {
+            foreach($ingredients as $ingredient) {
+                Ingredient::create([
+                    'recipe_id' => $recipeId,
+                    'name' => $ingredient['name'],
+                    'amount' => $ingredient['amount'],
+                    'measurement' => $ingredient['measurement']
+                ]);
+            }
+        }   
+    }
 }
