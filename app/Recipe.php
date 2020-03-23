6126357bbed9545d56ea6e20de46070d794b461d
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-03-23 10:54:30 -0500

    define recipe model relationships

--- app/Recipe.php
+++ app/Recipe.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    //
+    public function ingredients()
+    {
+        return $this->hasMany('App\Ingredient');
+    }
+
+    public function steps()
+    {
+        return $this->hasMany('App\Step');
+    }
 }
