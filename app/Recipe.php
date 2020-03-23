1101f0780e9e07ed8ffc7bbb9e15207f752e6a28
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-03-23 13:46:28 -0500

    add function comments

--- app/Recipe.php
+++ app/Recipe.php
@@ -8,16 +8,35 @@ class Recipe extends Model
 {
     protected $fillable = ['title', 'description'];
     
+    /**
+     * Relationship between Recipe and Ingredient
+     * Given a recipe, return the ingrdients
+     *
+     * @return void
+     */
     public function ingredients()
     {
         return $this->hasMany('App\Ingredient');
     }
 
+    /**
+     * Relationship between Recipe and Step
+     * Given a recipe, return the steps
+     *
+     * @return void
+     */
     public function steps()
     {
         return $this->hasMany('App\Step');
     }
 
+    /**
+     * Create Recipe object using title and description.
+     * Return the Recipe object
+     *
+     * @param [type] $recipe_info
+     * @return void
+     */
     public static function storeRecipe($recipe_info)
     {
         $recipe = Recipe::create([
