b34c07ac85c459e2faee625533fae5fff15afe76
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-03-27 12:58:13 -0500

    add user_id column to recipe table

--- database/migrations/2020_03_20_180457_create_recipes_table.php
+++ database/migrations/2020_03_20_180457_create_recipes_table.php
@@ -17,6 +17,7 @@ class CreateRecipesTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('description');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
--- database/seeds/RecipeSeeder.php
+++ database/seeds/RecipeSeeder.php
@@ -14,37 +14,44 @@ class RecipeSeeder extends Seeder
     {
         $recipe1 = Recipe::create([
             'title' => 'Tacos',
-            'description' => 'Ground beef tacos with all the fixins'
+            'description' => 'Ground beef tacos with all the fixins',
+            'user_id' => 1
         ]);
 
         $recipe2 = Recipe::create([
             'title' => 'Salmon',
-            'description' => 'Baked salmon with a dijon and lemon sauce'
+            'description' => 'Baked salmon with a dijon and lemon sauce',
+            'user_id' => 1
         ]);
 
         $recipe3 = Recipe::create([
             'title' => 'Fajitas',
-            'description' => 'Sheet pan chicken fajitas with peppers and onions'
+            'description' => 'Sheet pan chicken fajitas with peppers and onions',
+            'user_id' => 1
         ]);
 
         $recipe4 = Recipe::create([
             'title' => 'Spaghetti and Meatballs',
-            'description' => 'Spaghetti with hand rolled ground turkey meatballs'
+            'description' => 'Spaghetti with hand rolled ground turkey meatballs',
+            'user_id' => 2
         ]);
 
         $recipe5 = Recipe::create([
             'title' => 'Pizza',
-            'description' => 'Frozen pizza of your choice'
+            'description' => 'Frozen pizza of your choice',
+            'user_id' => 2
         ]);
 
         $recipe6 = Recipe::create([
             'title' => 'Beef and Broccoli',
-            'description' => 'Classic homemade chinese food'
+            'description' => 'Classic homemade chinese food',
+            'user_id' => 1
         ]);
 
         $recipe7 = Recipe::create([
             'title' => 'Breakfast Burrito',
-            'description' => 'Breakfast for dinner!'
+            'description' => 'Breakfast for dinner!',
+            'user_id' => 2
         ]);
     }
 }
