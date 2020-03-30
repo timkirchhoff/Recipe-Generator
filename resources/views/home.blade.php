15c574f6da09348315fab0204fa18382b29d9d40
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-03-30 12:49:17 -0500

    add basic nav with login and registration links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,26 @@
 @section('title')
     Recipe App
 @endsection
-        
+
 @section('content')
+    <!-- Header -->
+    <header id="header" class="alt">
+        @guest
+            <a href="{{ route('login') }}">Sign In</a>
+            <a href="{{ route('register') }}">Register</a>
+        @else
+            <a href="#">{{ Auth::user()->name }}</a>
+            <a href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        @endguest
+    </header>
+
     <!-- Banner -->
     <section id="banner">
         <div class="inner">
