061e812cce9b1e215beb9b3af4d7c6cb7cbaa93d
Tim Kirchhoff <tkirchhoff@gmail.com>
2020-04-15 12:58:03 -0500

    use SoftDeletes on Recipe model

--- app/Recipe.php
+++ app/Recipe.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Recipe extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['title', 'description', 'user_id'];
     
     /**
